3057cf214f7f7a441198763b2195df1c687ea454
chanhihi <chanhihi55@gmail.com>
2021-10-11 10:43:53 +0900

    Create 1316_그룹_단어_체커.swift

---
+++ BOJ/1316_그룹_단어_체커.swift
@@ -0,0 +1,30 @@
+//
+//  1316_그룹_단어_체커.swift
+//  
+//
+//  Created by Chan on 2021/10/11.
+//
+//  https://www.acmicpc.net/problem/1316
+
+import Foundation
+
+var output = 0
+let n = Int(readLine()!)!
+
+for _ in 0..<n {
+    var checker : [Character] = []
+    var s = readLine()!
+    
+    for c in s {
+        if !checker.contains(c){
+            checker.append(c)
+        } else if checker.last! != c{
+            checker.removeAll()
+            break
+        }
+    }
+
+    if checker.count != 0 { output += 1}
+
+    print(output)
+}
