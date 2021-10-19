37da709baf2a4fb7e2b4f0bc68bc9219643d03b2
chanhihi <chanhihi55@gmail.com>
2021-10-19 19:29:24 +0900

    Update 10250_ACM_호텔.swift

--- BOJ/10250_ACM_호텔.swift
+++ BOJ/10250_ACM_호텔.swift
@@ -7,40 +7,14 @@
     
 
 // MARK: -
-let T = 2 // Int(readLine()!)!
+let T = Int(readLine()!)!
 
-//for i in 1...T {
-//
-//}
-
-var n = "30 50 90".split(separator: " ").map { Int($0) }
-var (H, W, N) = (n[0]!, n[1]!, n[2]!)
-var output = ""
-W = (N / H) + 1
-var d = N % H
-if d == 0 { H = N / H }
-else { H = d }
-
-output.append(String(H))
-output.append(String("0"))
-output.append(String(W))
-
-print(output)
-
-
-// MARK: - 다시 풀어보기
-
-import Foundation
-
-let num = Int(readLine()!)!
-
-for _ in 1...num{
-    let testCase = readLine()!.components(separatedBy: " ").map{Double($0)!}
-    let share = ceil(testCase[2] / testCase[0])
-    var remainder = Int(testCase[2]) % Int(testCase[0])
-    if remainder == 0 {
-        remainder = Int(testCase[0])
-    }
-    let result = remainder * 100 + Int(share)
-    print(result)
+for _ in 1...T {
+    var n = readLine()!.split(separator: " ").map { Double($0)! }
+    var (H, W, N) = (n[0], n[1], n[2])
+    W = ceil(N / H)
+    var d = Int(N) % Int(H)
+    if d != 0 { H = Double(d) }
+    var output = Int(H * 100) + Int(W)
+    print(output)
 }
