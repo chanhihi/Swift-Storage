8e88869fe330c9d4b26ad2c053e517bc65bb45f5
chanhihi <chanhihi55@gmail.com>
2021-11-03 14:36:11 +0900

    Update 1463_1로_만들기.swift

--- BOJ/1463_1로_만들기.swift
+++ BOJ/1463_1로_만들기.swift
@@ -11,11 +11,12 @@
 var n = Int(readLine()!)!
 var dp = [Int](repeating: 0, count: n+1)
 for i in 2..<n+1 {
-    dp[i]=dp[i-1]+1
-    if i%3==0 {dp[i]=min(dp[i], dp[i/3]+1)}
-    if i%2==0 {dp[i]=min(dp[i], dp[i/2]+1)}
+    dp[i] = dp[i-1] + 1
+    if i % 3==0 { dp[i] = min(dp[i], dp[i/3]+1) }
+    if i % 2==0 { dp[i] = min(dp[i], dp[i/2]+1) }
 }
 print(n==1 ? 0 : dp[n])
+
 
 // MARK: - dp 초기값 1,2,3
 
