735134784824a3a8ae663884949834cdafa08ffb
chanhihi <chanhihi55@gmail.com>
2021-11-15 18:08:52 +0900

    Create _file_IO_Usage.swift

---
+++ BOJ/_file_IO_Usage.swift
@@ -0,0 +1,17 @@
+//
+//  _file_IO_Usage.swift
+//  
+//
+//  Created by Chan on 2021/11/15.
+//
+//  https://gist.github.com/JCSooHwanCho/30be4b669321e7a135b84a1e9b075f88
+
+// stdin Input: 1
+
+import Foundation
+
+let fIO = FileIO()
+
+let n = fIO.readInt()
+
+print(n) // 1
