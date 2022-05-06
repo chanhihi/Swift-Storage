c370c1f0a4c5e7110a0468cfad49f6adf8ddc496
chanhihi <chanhihi55@gmail.com>
2022-05-06 17:22:35 +0900

    Create enum_example.swift

---
+++ Garage/enum_example.swift
@@ -0,0 +1,39 @@
+//
+//  enum_example.swift
+//  
+//
+//  Created by Chan on 2022/05/06.
+//
+
+//enum ArithmeticExpression {
+//    case number(Int)
+//    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+//    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+//}
+
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let final = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .number(let value):
+        return value
+    case .addition(let left, let right):
+        return evaluate(left) + evaluate(right)
+    case .multiplication(let left, let right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+let result: Int = evaluate(final)
+print(result) // 18
+
+
