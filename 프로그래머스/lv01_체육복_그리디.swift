b57376bbb508c1aaf9f119a0bd7f24dbdea46a24
chanhihi <chanhihi55@gmail.com>
2022-05-19 16:43:45 +0900

    Create lv01_체육복_그리디.swift

---
+++ 프로그래머스/lv01_체육복_그리디.swift
@@ -0,0 +1,49 @@
+//
+//  그리디_체육복.swift
+//  
+//
+//  Created by Chan on 2021/12/02.
+//
+//	https://programmers.co.kr/learn/courses/30/lessons/42862?language=swift
+
+// MARK: - 배열을 사용한 풀이
+/// 재풀이 :
+
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var student = Array(repeating: 1, count: n)
+    var result = 0
+    
+    for j in 0..<reserve.count {
+        student[reserve[j]-1] += 1
+    }
+    
+    for i in 0..<lost.count {
+        student[lost[i]-1] -= 1
+    }
+    
+    for i in 0..<n {
+        if student[i] == 0 {
+            if (i-1) >= 0 && student[i-1] == 2 {
+                student[i-1] -= 1
+                student[i] += 1
+            } else if (i+1) < n && student[i+1] == 2  {
+                student[i+1] -= 1
+                student[i] += 1
+            }
+        }
+    }
+    
+    student.forEach {
+        if $0 > 0 {
+            result += 1
+        }
+    }
+    
+    return result
+}
+
+//5	[2, 4]	[1, 3, 5]	5
+//5	[2, 4]	[3]	4
+//3	[3]	[1]	2
