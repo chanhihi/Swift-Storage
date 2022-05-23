adfa5e3c4752ce60ed605e67785fb47e48b4cde4
chanhihi <chanhihi55@gmail.com>
2022-05-23 18:38:15 +0900

    Create lv01_두개_뽑아서_더하기.swift

---
+++ 프로그래머스/lv01_두개_뽑아서_더하기.swift
@@ -0,0 +1,23 @@
+//
+//  두개_뽑아서_더하기.swift
+//  
+//
+//  Created by Chan on 2021/11/28.
+//
+//	https://programmers.co.kr/learn/courses/30/lessons/68644?language=swift
+
+//   MARK: - 두개 뽑아서 더하기
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result: Set<Int> = []
+    
+    for i in 0..<numbers.count {
+        for j in i+1..<numbers.count {
+            result.insert(numbers[i]+numbers[j])
+        }
+    }
+    
+    return Array(result.sorted(by: <))
+}
