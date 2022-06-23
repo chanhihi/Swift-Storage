2af4e2e3fe850b2a96f6da9c1b164f0075097658
chanhihi <chanhihi55@gmail.com>
2022-06-24 01:10:18 +0900

    Create lv01_문자열을_정수로_바꾸기.swift

---
+++ 프로그래머스/lv01_문자열을_정수로_바꾸기.swift
@@ -0,0 +1,12 @@
+//
+//  lv01_문자열을_정수로_바꾸기.swift
+//  
+//
+//  Created by Chan on 2022/06/23.
+//
+//	https://programmers.co.kr/learn/courses/30/lessons/12925
+
+//	MARK: - Int로 형변환 과정에서 예외사항 없음.
+func solution(_ s:String) -> Int {
+	return Int(s)!
+}
