2300b28c6dd1a6e7e6a039bd5dfd06705ce72fd7
chanhihi <chanhihi55@gmail.com>
2022-06-23 17:11:04 +0900

    Create lv01_서울에서_김서방_찾기.swift

---
+++ 프로그래머스/lv01_서울에서_김서방_찾기.swift
@@ -0,0 +1,11 @@
+//
+//  lv01_서울에서_김서방_찾기.swift
+//  
+//
+//  Created by Chan on 2022/06/23.
+//
+//	https://programmers.co.kr/learn/courses/30/lessons/12919
+
+func solution(_ seoul:[String]) -> String {
+	return "김서방은 \(seoul.firstIndex(of:"Kim")!)에 있다"
+}
