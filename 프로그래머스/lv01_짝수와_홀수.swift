1257ef8b290c18453403f5f4001f21062e019e9e
chanhihi <chanhihi55@gmail.com>
2022-06-26 11:19:58 +0900

    Create lv01_짝수와_홀수.swift

---
+++ 프로그래머스/lv01_짝수와_홀수.swift
@@ -0,0 +1,11 @@
+//
+//  lv01_짝수와_홀수.swift
+//  
+//
+//  Created by Chan on 2022/06/26.
+//
+//	https://programmers.co.kr/learn/courses/30/lessons/12937
+
+func solution(_ num:Int) -> String {
+	return num % 2 == 0 ? "Even" : "Odd"
+}
