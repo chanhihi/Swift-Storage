051f560c4294b5a440155c7e3ab09fdf6c41477d
chanhihi <chanhihi55@gmail.com>
2022-09-01 13:51:30 +0900

    Create 15650_N과_M_(2).swift

---
+++ BOJ/15650_N과_M_(2).swift
@@ -0,0 +1,29 @@
+//
+//  15650_N과_M_(2).swift
+//  swift_practise
+//
+//  Created by Chan on 2022/09/01.
+//
+//	https://www.acmicpc.net/problem/15650
+
+//	MARK: - 재귀 / 백트래킹
+
+let l = readLine()!.split(separator: " ").map {Int(String($0))!}
+let (N, M) = (l[0], l[1])
+var result = [Int]()
+
+func visit() {
+	if result.count == M {
+		print(result.map{String($0)}.joined(separator :" "))
+		return
+	}
+	for i in 1...N {
+		if result.last ?? 0 < i {
+			result.append(i)
+			visit()
+			result.removeLast()
+		}
+	}
+}
+
+visit()
