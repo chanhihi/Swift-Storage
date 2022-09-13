1d35f0dbaa74765fa48f860c678f7e5fedd9b026
chanhihi <chanhihi55@gmail.com>
2022-09-13 21:36:19 +0900

    Create 17626_Four_Squares.swift

---
+++ BOJ/17626_Four_Squares.swift
@@ -0,0 +1,27 @@
+//
+//  17626_Four_Squares.swift
+//  swift_practise
+//
+//  Created by Chan on 2022/09/13.
+//
+//	https://www.acmicpc.net/problem/17626
+
+import Foundation
+
+extension Double {
+	func isInt() -> Bool {
+		return Double(floor(self)) == Double(self)
+	}
+}
+
+func solution(_ number: Double) -> Int {
+	if sqrt(number).isInt() {return 1}
+	let n = Int(sqrt(number))
+	for i in 0...n where sqrt(number-Double(i*i)).isInt() {return 2}
+	for i in 0...n {
+		for j in 0...Int(number-Double(i*i)) where sqrt(number-Double(i*i+j*j)).isInt() {return 3}
+	}
+	return 4
+}
+
+print(solution(Double(readLine()!)!))
