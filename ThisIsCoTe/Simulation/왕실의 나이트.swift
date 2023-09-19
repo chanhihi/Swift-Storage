0d6024698b59002512258192408f9830914f7a06
KIM CHANHEE <chanhihi55@gmail.com>
2023-09-20 00:24:35 +0900

    Update 왕실의 나이트.swift

--- ThisIsCoTe/Simulation/왕실의 나이트.swift
+++ ThisIsCoTe/Simulation/왕실의 나이트.swift
@@ -10,8 +10,6 @@ let r: [String: Int] = ["a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8]
 let NM = readLine()!.map{String($0)}
 let (N, M): (Int, Int) = (r[NM[0]]!, Int(NM[1])!)
 
-let map = Array(repeating: Array(repeating: 0, count: 8), count: 8)
-
 let dx = [2, 2, -2, -2, 1, 1, -1, -1]
 let dy = [1, -1, 1, -1, 2, -2, 2, -2]
 
