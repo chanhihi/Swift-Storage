bc6554e1f3e9d69f46d7d8d141fdd19ae0e1c353
chanhihi <chanhihi55@gmail.com>
2024-09-28 11:03:09 +0900

    solve: 14503

--- BOJ/_CPP/14503번： 로봇 청소기/로봇 청소기.cpp
+++ BOJ/_CPP/14503번： 로봇 청소기/로봇 청소기.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-// 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
-// 현재 칸의 주변
-// $4$칸 중 청소되지 않은 빈 칸이 없는 경우,
-// 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로
-// 돌아간다. 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
-// 현재 칸의 주변
-// $4$칸 중 청소되지 않은 빈 칸이 있는 경우,
-// 반시계 방향으로
-// $90^\circ$ 회전한다.
-// 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
-// 1번으로 돌아간다.
-
 int dx[] = {0, 1, 0, -1};
 int dy[] = {-1, 0, 1, 0};
 
@@ -23,13 +11,14 @@ int main() {
   cout.tie(nullptr);
 
   int n, m, r, c, d, site;
-  bool back = false;
 
   cin >> n >> m >> r >> c >> d;
 
   vector<vector<int>> board(n, vector<int>(m));
   for (int i = 0; i < n; i++) {
-    cin >> board[n][m];
+    for (int j = 0; j < m; j++) {
+      cin >> board[i][j];
+    }
   }
   site = 0;
 
@@ -39,34 +28,34 @@ int main() {
       site++;
     }
 
-    if (back) {
-      int tD = (2 + d) % 4;
-
-      int dm = r + dx[tD];
-      int dn = c + dy[tD];
-
-      if (board[dn][dm] == 1) {
-        break;
-      }
-
-      r = dm;
-      c = dn;
-    }
+    bool back = true;
 
     for (int i = 0; i < 4; i++) {
-      back = true;
+      d = (d + 3) % 4;
 
-      int dm = r + dx[d];
-      int dn = c + dy[d];
+      int dn = r + dy[d];
+      int dm = c + dx[d];
 
-      d = (3 + d) % 4;
-
-      if (board[dn][dm] == 0) {
+      if (dm >= 0 && dm < m && dn >= 0 && dn < n && board[dn][dm] == 0) {
         r = dn;
         c = dm;
         back = false;
         break;
       }
+    }
+
+    if (back) {
+      int tD = (d + 2) % 4;
+
+      int dn = r + dy[tD];
+      int dm = c + dx[tD];
+
+      if (dm >= 0 && dm < m && dn >= 0 && dn < n && board[dn][dm] == 1) {
+        break;
+      }
+
+      r = dn;
+      c = dm;
     }
   }
 
