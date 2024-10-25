9fe9ea5fb924a93f3db92d4c07934224cb6faa85
chanhihi <chanhihi55@gmail.com>
2024-10-25 19:55:10 +0900

    Create 17430

---
+++ BOJ/_CPP/17430번： 가로등/가로등.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 17430                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: chanhihi <boj.kr/u/chanhihi>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/17430                          #+#        #+#      #+#    */
+/*   Solved: 2024/10/25 19:50:33 by chanhihi      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int t, n;
+
+int main() {
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    cin >> n;
+    for (int j = 0; j < n; j++) {
+    }
+  }
+  //
+  cout << "BALANCED";
+  cout << "NOT BALANCED";
+  return 0;
+}
