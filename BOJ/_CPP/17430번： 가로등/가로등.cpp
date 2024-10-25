97303074c1074492512f71576dd54c2c53e46492
chanhihi <chanhihi55@gmail.com>
2024-10-25 15:24:54 +0900

    Solved 17430

--- BOJ/_CPP/17430번： 가로등/가로등.cpp
+++ BOJ/_CPP/17430번： 가로등/가로등.cpp
@@ -11,20 +11,38 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <map>
 #include <vector>
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
 
 using namespace std;
 int t, n;
 
 int main() {
+  fastio;
   cin >> t;
   for (int i = 0; i < t; i++) {
     cin >> n;
+    bool flag = false;
+    map<int, vector<int>> m;
+
     for (int j = 0; j < n; j++) {
+      int k, v;
+      cin >> k >> v;
+      m[k].push_back(v);
     }
+    for (auto &i : m) sort(i.second.begin(), i.second.end());
+    for (auto i : m) {
+      if (flag) break;
+      for (auto j : m) {
+        if (i.first == j.first) continue;
+        if (i.second != j.second) {
+          flag = true;
+          break;
+        }
+      }
+    }
+    cout << (flag ? "NOT BALANCED\n" : "BALANCED\n");
   }
-  //
-  cout << "BALANCED";
-  cout << "NOT BALANCED";
   return 0;
 }
